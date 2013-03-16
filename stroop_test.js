014a214addebfc3a5ed5d1426c72c114a2042568
Filipe Ximenes <filipeximenes@gmail.com>
2013-03-16 11:31:38 -0300

    organizing

--- stroop_test.js
+++ stroop_test.js
@@ -1,3 +1,81 @@
+// -------------------------------------------------------------------
+// -------------------------- Games ----------------------------------
+// -------------------------------------------------------------------
+/* 
+    All kinds of game should insteract with the game area throught 
+    a interface containing the methods:
+
+    * start_game() - the method called after the count down ends.
+
+    * controller((boolean) result) - is a callback method, called after 
+    each answer given by the user.
+
+    When the game ends if should call the log_result function 
+    with a GameLog object.
+*/
+
+// ----------------------- Stroop Off --------------------------------
+
+function StroopOff(number_of_collors, number_of_rounds){
+    get_color_key_list(number_of_collors);
+    this.number_of_rounds = number_of_rounds;
+    this.round = 0;
+    this.current_collor = null;
+    this.started_att = null;
+    this.ended_att = null;
+}
+
+StroopOff.prototype.start_game = function(){
+    this.started_att = new Date();
+    this.game();
+}
+
+StroopOff.prototype.controller = function(chosen_color){
+    if (chosen_color === this.current_collor){
+        if (this.round < this.number_of_rounds){
+            this.game();
+        }else{
+            this.ended_att = new Date();
+            res = new GameLog(this.get_total_time(), true)
+            log_result(res);
+        }
+    }else{
+        this.ended_att = new Date();
+        res = new GameLog(this.get_total_time(), false)
+        log_result(res);
+    }
+}
+
+StroopOff.prototype.game = function(){
+    this.round += 1
+    clear_game_area()
+    this.current_collor = get_random_key();
+    $("#test_area").append(generate_formated_text(this.current_collor))
+    populate_selectors_area()
+}
+
+StroopOff.prototype.get_total_time = function(){
+    return (this.ended_att - this.started_att) / 1000
+}
+
+// ----------------------- Stroop On ---------------------------------
+
+/*
+ * TODO
+*/
+
+// -------------------------------------------------------------------
+// ---------------------------- Logs ---------------------------------
+// -------------------------------------------------------------------
+
+function GameLog(total_time, result){
+    this.total_time = total_time;
+    this.result = result;
+}
+
+// -------------------------------------------------------------------
+// ---------------------------- Utils --------------------------------
+// -------------------------------------------------------------------
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -9,11 +87,10 @@ function shuffleArray(array) {
     return array;
 }
 
-var color_names = {'Vermelho': 'red', 'Azul': 'blue', 'Verde': 'green', 'Preto': 'black'}
-var color_keys = []
-
-var game_playing = null
-var logs = []
+function get_random_key(){
+    color_list = get_color_key_list()
+    return color_list[Math.floor(Math.random() * color_list.length)]
+}
 
 function get_color_key_list(size){
     if (size !== undefined && color_keys !== null && color_keys.length !== size){
@@ -34,14 +111,41 @@ function get_color_key_list(size){
     return color_keys    
 }
 
-function get_random_key(){
-    color_list = get_color_key_list()
-    return color_list[Math.floor(Math.random() * color_list.length)]
+// -------------------------------------------------------------------
+// ------------------------ Game Area --------------------------------
+// -------------------------------------------------------------------
+
+var color_names = {'Vermelho': 'red', 'Azul': 'blue', 'Verde': 'green', 'Preto': 'black'}
+var color_keys = []
+
+var game_playing = null
+var logs = []
+
+function start_test(){
+    clear_game_area()
+    $("#start_button").hide();
+    $("#selectors_area").show();
+    game_playing = new StroopOff(3, 10)
+    play_count_down(3)
 }
 
 function clear_game_area(){
     $("#test_area").html('');
     $("#selectors_area").html('');
+}
+
+function populate_selectors_area(){
+    color_list = get_color_key_list().slice(0)
+    color_list = shuffleArray(color_list)
+
+    for (key in color_list){
+        button = $("<button></button>");
+        button.html(color_list[key]);
+        button.click(function(){
+            game_playing.controller($(this).text())
+        })
+        $("#selectors_area").append(button);
+    }
 }
 
 function generate_formated_text(color_key, stroop){
@@ -58,29 +162,6 @@ function generate_formated_text(color_key, stroop){
     span_tag.css('color', chosen_color)
 
     return span_tag
-}
-
-function start_test(){
-    clear_game_area()
-    $("#start_button").hide();
-    $("#selectors_area").show();
-    game_playing = new StroopOff(3, 10)
-    play_count_down(3)
-}
-
-function show_result(){
-    for (key in logs){
-        $("#test_area").append(logs[key].total_time + '<br>')
-    }
-}
-
-function verify_answer(value){
-    clear_game_area()
-    if (value === current_collor){
-        game_playing.controller(true)
-    }else{
-        game_playing.controller(false)
-    }
 }
 
 function play_count_down(showing_time, count_down){
@@ -103,74 +184,18 @@ function play_count_down(showing_time, count_down){
     }
 }
 
-function populate_selectors_area(){
-    color_list = get_color_key_list().slice(0)
-    color_list = shuffleArray(color_list)
-
-    for (key in color_list){
-        button = $("<button></button>");
-        button.html(color_list[key]);
-        button.click(function(){
-            verify_answer($(this).text())
-        })
-        $("#selectors_area").append(button);
+function show_result(){
+    clear_game_area()
+    $("#start_button").show();
+    $("#selectors_area").hide();
+    for (key in logs){
+        $("#test_area").append(logs[key].total_time + '<br>')
     }
 }
 
 function log_result(log){
-    logs.push(log)
-
-    clear_game_area()
-    $("#start_button").show();
-    $("#selectors_area").hide();
-    
+    logs.push(log)    
     show_result()
-}
-
-function GameLog(total_time, result){
-    this.total_time = total_time;
-    this.result = result;
-}
-
-function StroopOff(number_of_collors, number_of_rounds){
-    get_color_key_list(number_of_collors);
-    this.number_of_rounds = number_of_rounds;
-    this.round = 0;
-    this.current_collor = null;
-    this.started_att = null;
-    this.ended_att = null;
-}
-
-StroopOff.prototype.start_game = function(){
-    this.started_att = new Date();
-    this.game();
-}
-
-StroopOff.prototype.controller = function(result){
-    if (result){
-        if (this.round < this.number_of_rounds){
-            this.game();
-        }else{
-            this.ended_att = new Date();
-            res = new GameLog(this.get_total_time(), true)
-            log_result(res);
-        }
-    }else{
-        this.ended_att = new Date();
-        res = new GameLog(this.get_total_time(), false)
-        log_result(res);
-    }
-}
-
-StroopOff.prototype.game = function(){
-    this.round += 1
-    current_collor = get_random_key();
-    $("#test_area").append(generate_formated_text(current_collor))
-    populate_selectors_area()
-}
-
-StroopOff.prototype.get_total_time = function(){
-    return (this.ended_att - this.started_att) / 1000
 }
 
 $("#start_button").click(start_test);
